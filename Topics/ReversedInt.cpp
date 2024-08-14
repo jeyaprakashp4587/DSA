e9c90998c4121e931e0c8e111cc904f02125f495
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-08-14 14:33:38 +0530

    rr

--- Topics/ReversedInt.cpp
+++ Topics/ReversedInt.cpp
@@ -3,14 +3,19 @@ using namespace std;
 int main() {
     int numbers;
     cin>>numbers;
-    int reversed = 0;
+    long long reversed = 0;
     while (numbers !=0){
         int digit = numbers % 10;
         reversed  = reversed *10 + digit;
+         if (reversed > INT_MAX || reversed < INT_MIN) {
+                return 0;
+            }
         numbers /= 10;
         while (reversed %10 == 0 && reversed != 0) {
         reversed /= 10;  
         }
+        int resukt =  static_cast<int>(reversed);
+        cout<<resukt<<endl;
     }
     return 0;
 }
