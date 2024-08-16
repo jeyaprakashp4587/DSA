647ecc674496389c54ce93aef7488b0356b12f55
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-08-16 21:48:41 +0530

    long

--- Topics/longestCommonPrefix.cpp
+++ Topics/longestCommonPrefix.cpp
@@ -9,16 +9,17 @@ int main() {
     // sort(arr.begin(),arr.end());
     string firstElement = arr[0];
      string lastElement = arr[arr.size() -1];
-    for (int i = 0; i < firstElement.size(); i++)
-    { 
-     if (firstElement[i] == lastElement[i])
-     {
-       result +=firstElement[i];
-     }
-     else{
-        break;
-     }
-    }
-    cout<<result;
+    // for (int i = 0; i < firstElement.size(); i++)
+    // { 
+    // //  if (firstElement[i] == lastElement[i])
+    // //  {
+    // //    result +=firstElement[i];
+    // //  }
+    // //  else{
+    // //     break;
+    // //  }
+
+    // }
+    cout<<arr[1];
     return 0;
 }
