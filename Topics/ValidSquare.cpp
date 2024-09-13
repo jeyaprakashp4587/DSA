0518faaa36d00fca3a02471f3d18b987d9b3ac8e
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-09-13 11:58:06 +0530

    validSquare

---
+++ Topics/ValidSquare.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n = 16;
+    int sqr = 0;
+    bool result;
+    for (int i = 1; i * i <= n; i++)
+    {
+        sqr = i;
+    }
+
+    if (sqr * sqr == n)
+    {
+        result = true;
+    }
+    cout << result;
+    return 0;
+}
