cb2faa05b50189623567ea07ad9f9c218efa83aa
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-10-02 15:48:11 +0530

    linked

--- Topics/Java/Main.java
+++ Topics/Java/Main.java
@@ -18,12 +18,18 @@
   // Node newNode
   public void addBegin(int data) {
     Node newNode = new Node(data);
-    newNode.next =head;
+    System.out.println("data"+data);
+    newNode.next = head;
+    System.out.println("next"+newNode.data);
+    head = newNode;
+    System.out.println("newNode"+newNode.next);
   }
 }
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("d");
+        LinkedList list = new LinkedList();
+        list.addBegin(5);
     }
 }
