53667c9dacdf48bcac2c478c791064ed2ae2015f
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-10-02 15:51:59 +0530

    main

--- Topics/Java/Main.java
+++ Topics/Java/Main.java
@@ -5,7 +5,7 @@
   int data;
   Node next;
   Node(int data){
-    this.data =data;
+    this.data = data;
     this.next = null;
   }
 }
@@ -18,18 +18,31 @@
   // Node newNode
   public void addBegin(int data) {
     Node newNode = new Node(data);
-    System.out.println("data"+data);
+    // System.out.println("data"+data);
     newNode.next = head;
-    System.out.println("next"+newNode.data);
+    // System.out.println("next"+newNode.data);
     head = newNode;
-    System.out.println("newNode"+newNode.next);
+    // System.out.println("newNode"+newNode.next);
+  }
+  // display
+  public void display() {
+    Node current = head;
+    while (current != null) {
+      System.out.print(current.data+"->");
+      current = current.next;
+    }
   }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("d");
+      
         LinkedList list = new LinkedList();
-        list.addBegin(5);
+       
+         list.addBegin(5);
+          list.addBegin(2);
+           list.addBegin(9);
+            list.addBegin(6);
+        list.display();
     }
 }
