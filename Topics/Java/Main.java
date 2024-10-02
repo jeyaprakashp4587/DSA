72bd42b5d9732ed720257b320f425f13b9a36b36
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-10-02 17:02:43 +0530

    java

--- Topics/Java/Main.java
+++ Topics/Java/Main.java
@@ -18,19 +18,32 @@
   // Node newNode
   public void addBegin(int data) {
     Node newNode = new Node(data);
-    // System.out.println("data"+data);
     newNode.next = head;
-    // System.out.println("next"+newNode.data);
     head = newNode;
-    // System.out.println("newNode"+newNode.next);
+    // System.out.println("head"+head);
+    //  System.out.println("data"+newNode.data);
+    //   System.out.println("next"+newNode.next);
+    //   System.out.println("==========");
   }
   // display
   public void display() {
     Node current = head;
     while (current != null) {
-      System.out.print(current.data+"->");
+      System.out.print(" "+current.data+"->"+current.next);
       current = current.next;
     }
+  }
+  // insert position
+  public void addindex (int index,int data) {
+    Node newNode = new Node(data);
+
+    Node current = head;
+    
+    for(int i=0;i<index-1;i++){
+     current = current.next;
+    }
+    newNode.next = current;
+    current.next = newNode;
   }
 }
 
@@ -38,11 +51,10 @@ public class Main {
     public static void main(String[] args) {
       
         LinkedList list = new LinkedList();
-       
          list.addBegin(5);
-          list.addBegin(2);
-           list.addBegin(9);
-            list.addBegin(6);
-        list.display();
+          list.addBegin(10);
+          list.addBegin(1);
+          list.display();
+          list.addindex(2, 2);
     }
 }
