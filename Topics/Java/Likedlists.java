7be1ba52c08728bac671f55f6ac47e35364fa9c3
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-10-04 18:57:17 +0530

    list

--- Topics/Java/Likedlists.java
+++ Topics/Java/Likedlists.java
@@ -1,79 +1,106 @@
 /**
- * InnerLinkedList
+ * Likedlists
  */
- class Node {
+// create linked list
+/**
+ * Node
+ */
+class Node {
   int data;
   Node next;
-  Node(int data){
+
+  public Node(int data) {
     this.data = data;
     this.next = null;
   }
 }
 
 /**
- * InnerMain
- */
- class LinkedList {
+* 
+*/
+class Linked {
   Node head;
-  // Node newNode
-  public void addBegin(int data) {
+
+  public void addNode(int data) {
     Node newNode = new Node(data);
     newNode.next = head;
     head = newNode;
-    // System.out.println("head"+head);
-    //  System.out.println("data"+newNode.data);
-    //   System.out.println("next"+newNode.next);
-    //   System.out.println("==========");
   }
-  // display
-  public void display() {
+
+  public void displayNodes() {
+    Node temp = head;
+    if (temp == null) {
+      return;
+    }
+    while (temp != null) {
+      System.out.println("data" + temp.data + "->" + "nextNode" + temp.next);
+      temp = temp.next;
+    }
+  }
+
+  public void insertData(int data, int index) {
+    // if (index) {
+    Node Newnode = new Node(data);
+    if (index == 0) {
+      Newnode.next = head;
+      head = Newnode;
+      return;
+    }
     Node current = head;
-    while (current != null) {
-      System.out.print(" "+current.data+"->"+current.next);
+    for (int i = 0; i < index - 1; i++) {
       current = current.next;
     }
+    // System.out.println("current Node" + current.next);
+    Newnode.next = current.next;
+    current.next = Newnode;
   }
-  // insert position
-  public void addindex (int index,int data) {
 
-        if (index < 0) {
-            System.out.println("Index cannot be negative.");
-            return;
-        }
-
-        Node newNode = new Node(data);
-
-        if (index == 0) {
-            newNode.next = head;
-            head = newNode;
-            return;
-        }
-int cnt=0;
-        Node current = head;
-        for (int i = 0; i < index - 1; i++) {
-            if (current == null) {
-                System.out.println("Index out of bounds.");
-                return;
-            }
-            current = current.next;
-            cnt++;
-        }
-System.out.println(cnt);
-        newNode.next = current.next;
-        current.next = newNode;
-}
- }
-
-public class Likedlists {
-    public static void main(String[] args) {
-      
-        LinkedList list = new LinkedList();
-         list.addBegin(5);
-          list.addBegin(10);
-          list.addBegin(1);
-            list.addindex(2,2);
-            list.addindex(3,19);
-          list.display();
-        
+  // get length
+  public int getSize() {
+    int count = 0;
+    Node temp = head;
+    while (temp != null) {
+      temp = temp.next;
+      count++;
     }
+    // System.out.println("Linked List Size" + count);
+    return count;
+  }
+
+  //
+  public void insertAtLast(int data) {
+    Node newNode = new Node(data); // Create a new node
+
+    // If the list is empty, the new node becomes the head
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+
+    // Traverse to the last node
+    Node current = head;
+    while (current.next != null) {
+      current = current.next;
+      System.out.println("curretn" + current.data);
+    }
+
+    // Set the next of the last node to the new node
+    current.next = newNode;
+
+  }
+}
+
+//
+public class Likedlists {
+  public static void main(String[] args) {
+    Linked link = new Linked();
+    link.addNode(5);
+    link.addNode(10);
+    // link.insertData(2, 2);
+    link.insertData(1, 1);
+    link.insertData(6, 2);
+    link.insertAtLast(4);
+    link.displayNodes();
+    // link.getSize();
+  }
 }
